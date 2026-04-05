771fc82ae04551d59f48363163af988cf6487b82
prabhatxmishra <prabhatmishra2402@gmail.com>
2026-04-05 22:49:54 +0530

    Initial commit: project setup with Spring Boot

---
+++ src/main/java/com/prabhatxmishra/autolife/AutolifeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prabhatxmishra.autolife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolifeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutolifeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prabhatxmishra/autolife/AutolifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabhatxmishra.autolife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutolifeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
